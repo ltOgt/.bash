48e6594a7321583d036fbd05cda7a226215958c8
omni <omnesiaorg@gmail.com>
2019-12-11 01:47:22 +0100

    add branch info

--- git.bash
+++ git.bash
@@ -28,3 +28,15 @@ gitd ()
 	fi
 }
 
+
+# branch name in console
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+if [ -f /etc/bash_completion ]; then
+  . /etc/bash_completion
+	#   																						user  machine           path                                git
+	export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)$ '
+else
+	echo "bash-completion not installed!"
+fi
