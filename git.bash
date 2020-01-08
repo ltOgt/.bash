f2b5a9736cfc86569f495767f5efa20dd00ce819
omni <omnesiaorg@gmail.com>
2020-01-08 13:56:18 +0100

    add branch to terminal

--- git.bash
+++ git.bash
@@ -35,8 +35,29 @@ parse_git_branch() {
 }
 if [ -f /etc/bash_completion ]; then
   . /etc/bash_completion
-	#   																						user  machine           path                                git
-	export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)$ '
+
+	# extracted from ./ubuntu.bash
+	# set a fancy prompt (non-color, unless we know we "want" color)
+	case "$TERM" in
+		xterm-color|*-256color) color_prompt=yes;;
+	esac
+	if [ "$color_prompt" = yes ]; then
+		#   																						user  machine           path                                git
+		export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)$ '
+	else
+		PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+	fi
+	unset color_prompt
 else
 	echo "bash-completion not installed!"
 fi
+
+	# If this is an xterm set the title to user@host:dir
+	#case "$TERM" in
+	#xterm*|rxvt*)
+#		PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+#		;;
+#	*)
+#		;;
+#	esac
+
