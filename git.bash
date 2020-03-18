bae66242fba2e24a8adcf67477b72c30e460eba0
ltogt <omnesiaorg@gmail.com>
2020-03-18 01:24:01 +0100

    new line prompt after branch when in git repo

--- git.bash
+++ git.bash
@@ -33,6 +33,9 @@ gitd ()
 parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
+generate_git_delim() {
+	[[ "$(git branch 2> /dev/null)" == "" ]] && echo "$" || echo -e "\n$"
+}
 if [ -f /etc/bash_completion ]; then
   . /etc/bash_completion
 
@@ -43,7 +46,9 @@ if [ -f /etc/bash_completion ]; then
 	esac
 	if [ "$color_prompt" = yes ]; then
 		#   																						user  machine           path                                git
-		export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\n$ '
+		delim=$([[ "$(git branch 2> /dev/null)" == "" ]] && echo "\n$" || echo "$")
+		export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)$(generate_git_delim) '
+		unset delim
 	else
 		PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 	fi
