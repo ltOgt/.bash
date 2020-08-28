48737d896795da0f659ce2249bed5182cbf8bfb6
ltogt <omnesiaorg@gmail.com>
2020-08-29 01:01:40 +0200

    quick create for packages

--- flutter.bash
+++ flutter.bash
@@ -27,3 +27,45 @@ flutter_build_apk () {
 _flutter_build_apk () {
 	flutter clean && flutter build apk
 }
+
+flutter_new_package() {
+	if [ $# -ne 1 ]; then
+		echo "flutter_new_package <name>"
+		return 1
+	fi
+
+	echo ""
+	echo "{{{ CREATING PACKAGE FROM TEMPLATE }}}"
+	flutter create --template=package $1
+	cd ./$1
+
+	echo ""
+	echo "{{{ INITIALIZE REPO }}}"
+	git init
+
+	echo ""
+	echo "{{{ CLEAR README }}}"
+	echo "#$1" > README.md
+
+	echo "{{{ COMMIT INIT }}}"
+	git add .
+	git commit -m "Init"
+
+	echo ""
+	echo "{{{ ADD REMOTE }}}"
+	repo=git@github.com:ltOgt/$1.git
+	git remote add origin $repo
+	git push --set-upstream origin master && return 0
+
+	echo ""
+	echo "please create repo at $repo"
+	echo -n "done? [y/n] "
+	read response
+	if [[ $response == "y" || $response == "Y" ]]; then
+		git push --set-upstream origin master
+		return 0
+	fi
+	return 1
+}
+
+alias flutterweb='/home/omni/repos/flutter_web/flutter/bin/flutter'
